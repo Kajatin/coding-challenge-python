a7540131d2193d7d6acf1b3b8e2f21e9afc2df95
Roland Kajatin <roland.kajatin@gmail.com>
2020-10-02 19:52:52 +0200

    Two solutions for the coding challenge. Removed the unnecessary pyc file

--- colors.py
+++ colors.py
@@ -1,52 +1,78 @@
 from argparse import ArgumentParser
 from getColor import getColor
 
-class Color(object): 
-    def __init__(self, name):
-            self.name = name
-        
-class Red(Color): 
-  def __init__(self, name):
-      Color.__init__(self, "red")
-      
-class Green(Color): 
-  def __init__(self, name):
-      Color.__init__(self, "green")
 
-def build_parser():
-    parser = ArgumentParser()
-    parser.add_argument('--green', dest='green', default="")
-    parser.add_argument('--blue', dest='blue')
-    parser.add_argument('--red', dest='red')
-    parser.add_argument('--order', dest='order',  nargs="+", required=True)
-    return parser
+class Color(object):
+    """ A class to store color information.
+
+    Attributes:
+    ----------
+    name: str
+        Name of the color.
+    HEX: str
+        Hexadecimal representation of the color.
+    RGB: dict
+        A dictionary with the R, G, and B values of the color.
+    """
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', 'N/A')
+        self.hex = kwargs.get('HEX', 'N/A')
+        self.rgb = kwargs.get('RGB', 'N/A')
+
+    def __repr__(self):
+        return 'Color: {},\tHEX: {},\tRGB: {}'.format(
+            self.name,
+            self.hex,
+            self.rgb)
+
 
 def main():
-    parser = build_parser()
-    options = parser.parse_args()
-    getColors(
-        options.green,
-        options.blue,
-        options.red,
-        options.order,
-        output
-        )
+    # set up a simple arg parser
+    parser = ArgumentParser(description='Script to display color information.')
+    parser.add_argument('order', nargs='+', help='order of colors to be displayed')
+    args = parser.parse_args()
     
-def output(colors):
-    hexColors = []
-    print(colors)
-    for x in colors:
-        hexColors.append(x["HEX"])
-    print("Red " + hexColors[0])
-    print("Green " + hexColors[1])
+    # minimal solution, not using classes
+    minimal(args.order)
+    
+    # a solution using class instances
+    using_classes(args.order)
 
-def getColors(green,blue,red,order,callback):
-    colors = []
-    if green: colors.insert(order.index("green"),getColor("green"))
-    if blue: colors.insert(order.index("blue"),getColor("blue"))
-    if red: colors.insert(order.index("red"),getColor("red"))
-    callback(colors)
-    return colors
+
+def using_classes(order):
+    """ Prints color information using class instances.
+
+    Parameters
+    ----------
+    order: list
+        A list of color names, e.g. ['red', 'white'].
+    """
+
+    # create a list of class instances for each valid `color`
+    colors = [Color(**getColor(color)) for color in order if getColor(color) is not None]
+    # print the color information for each class instance
+    for color in colors:
+        print(color)
+
+
+def minimal(order):
+    """ Prints color information using a lambda function directly.
+
+    Parameters
+    ----------
+    order: list
+        A list of color names, e.g. ['red', 'white'].
+    """
+
+    # lambda function to print color information
+    color_print = lambda info: print('Color: {},\tHEX: {},\tRGB: {}'.format(
+                info.get('name','N/A'),
+                info.get('HEX','N/A'),
+                info.get('RGB','N/A')))
+
+    # print color information for each valid `color`
+    [color_print(getColor(color)) for color in order if getColor(color) is not None]
 
 
 if __name__ == '__main__':
